be645fae84d8e106df540877fb0388390d4695e9
Sajjad Anwar <me@sajjad.in>
2014-02-12 19:40:57 +0530

    Adding all moabi layers to the default layer control

--- vendor/assets/leaflet/leaflet.osm.js
+++ vendor/assets/leaflet/leaflet.osm.js
@@ -1,4 +1,5 @@
 L.OSM = {};
+L.Moabi = {};
 
 L.OSM.TileLayer = L.TileLayer.extend({
   options: {
@@ -260,5 +261,71 @@ L.Util.extend(L.OSM, {
     }
 
     return result;
+  }
+});
+
+// Moabi Layers
+
+L.Moabi.Base = L.OSM.TileLayer.extend({
+  options: {
+    url: 'http://tiles.osm.moabi.org/moabi_base/{z}/{x}/{y}.png',
+    subdomains: '1234',
+    attribution: "© Moabi"
+  }
+});
+
+L.Moabi.Energy = L.OSM.TileLayer.extend({
+  options: {
+    url: 'http://tiles.osm.moabi.org/moabi_energy/{z}/{x}/{y}.png',
+    subdomains: '1234',
+    attribution: "© Moabi"
+  }
+});
+
+L.Moabi.Indigenous = L.OSM.TileLayer.extend({
+  options: {
+    url: 'http://tiles.osm.moabi.org/moabi_indigenous/{z}/{x}/{y}.png',
+    subdomains: '1234',
+    attribution: "© Moabi"
+  }
+});
+
+L.Moabi.Logging = L.OSM.TileLayer.extend({
+  options: {
+    url: 'http://tiles.osm.moabi.org/moabi_logging/{z}/{x}/{y}.png',
+    subdomains: '1234',
+    attribution: "© Moabi"
+  }
+});
+
+L.Moabi.Mining = L.OSM.TileLayer.extend({
+  options: {
+    url: 'http://tiles.osm.moabi.org/moabi_mining/{z}/{x}/{y}.png',
+    subdomains: '1234',
+    attribution: "© Moabi"
+  }
+});
+
+L.Moabi.Oil = L.OSM.TileLayer.extend({
+  options: {
+    url: 'http://tiles.osm.moabi.org/moabi_oil/{z}/{x}/{y}.png',
+    subdomains: '1234',
+    attribution: "© Moabi"
+  }
+});
+
+L.Moabi.Palm = L.OSM.TileLayer.extend({
+  options: {
+    url: 'http://tiles.osm.moabi.org/moabi_palm/{z}/{x}/{y}.png',
+    subdomains: '1234',
+    attribution: "© Moabi"
+  }
+});
+
+L.Moabi.Transport = L.OSM.TileLayer.extend({
+  options: {
+    url: 'http://tiles.osm.moabi.org/moabi_transport/{z}/{x}/{y}.png',
+    subdomains: '1234',
+    attribution: "© Moabi"
   }
 });
