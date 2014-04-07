5f221aedebd7966751a409ebf0a0355439a5f11d
Tim Waters <chippy2005@gmail.com>
2014-04-07 17:23:27 +0100

    Add useful scopes to tile model for basemaps, overlays etc

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,2 +1,5 @@
 class Tile < ActiveRecord::Base
+   scope :base_layers, -> { where.not(:base_layer => nil) }
+   scope :default_base_layers, -> { base_layers.where(:base_layer => "default")}
+   scope :overlays, -> {where(:base_layer => nil)}
 end
