e9a1e1dc9ada0c7d56fcd1b7ffd7a95514881bf5
Tim Waters <chippy2005@gmail.com>
2014-04-14 16:40:06 +0100

    Add validation to essential tile attributes. Add tile unit tests.

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -1,4 +1,6 @@
 class Tile < ActiveRecord::Base
+   validates :code,:keyid, :name, :url, :presence => true
+ 
    scope :base_layers, -> { where.not(:base_layer => nil) }
    scope :default_base_layers, -> { base_layers.where(:base_layer => "default")}
    scope :overlays, -> {where(:base_layer => nil)}
