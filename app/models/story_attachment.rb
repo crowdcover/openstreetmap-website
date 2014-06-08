2d7cc4ebe9999c7c2b126fa04203589b5916d059
Bryan Swift <mail@bryanwrit.es>
2014-06-08 16:52:08 -0700

    Create the StoryAttachment model object
    See crowdcover/crowdcover#214

---
+++ app/models/story_attachment.rb
@@ -0,0 +1,20 @@
+class StoryAttachment < ActiveRecord::Base
+  has_attached_file :image,
+    :styles => { :large => "1024x768#", :small => "320x240#" },
+    :default_url => "/assets/:class/:attachment/:style.gif",
+    :default_style => :large,
+    :convert_options => {
+      :large => '-strip',
+      :small => '-strip'
+    }
+
+  before_post_process :image?
+
+  validates_attachment_content_type :image,
+    :content_type => /\Aimage\/.*\Z/
+
+  def image?
+    !(image_content_type =~ /\Aimage\/.*\Z/)
+  end
+
+end
