667abe9962bf98cf6e9eaf9d5830948c3a108f0f
Tim Waters <chippy2005@gmail.com>
2014-10-26 15:05:11 +0000

    fix for permissions test

--- test/unit/permission_test.rb
+++ test/unit/permission_test.rb
@@ -59,23 +59,35 @@ class PermissionTest < ActiveSupport::TestCase
   #in concession=mining preset:
   #k=website is ok
   #k=operator is restricted
-  def test_create_node_local_forbidden
+  def test_create_node_local_allowed_more
     node_xml = "<osm><node id='23' lat='23.43' lon='23.32' changeset='#{changesets(:public_user_first_change).id}' version='32'><tag k='concession' v='mining' /><tag k='website' v='osm.org' /><tag k='operator' v='digalot inc' /></node></osm>"
 
     node = Node.from_xml(node_xml, true)
     
-    assert_raise(OSM::APIForbiddenError) {
+    assert_nothing_raised(OSM::APIForbiddenError) {
       node.create_with_history(users(:public_user))
     }
   end
   
-  def test_create_node_local_forbidden_other_user
+  #another user in the allowed group
+  def test_create_node_local_allowed_other_user
     node_xml = "<osm><node id='23' lat='23.43' lon='23.32' changeset='#{changesets(:second_public_user_first_change).id}' version='32'><tag k='concession' v='mining' /><tag k='website' v='osm.org' /><tag k='operator' v='digalot inc' /></node></osm>"
 
     node = Node.from_xml(node_xml, true)
-    
-    assert_raise(OSM::APIForbiddenError) {
+
+    assert_nothing_raised(OSM::APIForbiddenError) {
       node.create_with_history(users(:second_public_user))
+    }
+  end
+  
+    #another user in the allowed group
+  def test_create_node_local_forbidden
+    node_xml = "<osm><node id='23' lat='23.43' lon='23.32' changeset='#{changesets(:moderator_user_first_change).id}' version='32'><tag k='concession' v='mining' /><tag k='website' v='osm.org' /><tag k='operator' v='digalot inc' /></node></osm>"
+
+    node = Node.from_xml(node_xml, true)
+
+    assert_raise(OSM::APIForbiddenError) {
+      node.create_with_history(users(:moderator_user))
     }
   end
     
